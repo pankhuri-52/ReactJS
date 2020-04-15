330a8b34c42fb14b9838fe42e428591642886b52
Pankhuri <pankhuritrikha@gmail.com>
2020-04-15 16:25:46 +0530

    Done with the project

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   state = {
     counters : [
-        { id : 1, value : 4},
+        { id : 1, value : 0},
         { id : 2, value : 0},
         { id : 3, value : 0},
         { id : 4, value : 0}
